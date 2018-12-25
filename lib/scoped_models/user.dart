b0dc9b16b3e4bad7d366565f18d620b3141c2e30
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2018-12-25 05:03:59 -0200

    Added integration with firebase, for saving the data.

--- lib/scoped_models/user.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_course/models/user.dart';
-import 'package:flutter_course/scoped_models/connected_products.dart';
-
-mixin UserModel on ConnectedProductsModel {
-  void login(String email, String password) {
-    authenticatedUser = User(id: 'adfadf', email: email, password: password);
-  }
-}
